84e35f5dd9406add4f997c5e2f2f1efe6fece3ff
Bbanez <banezprod@gmail.com>
2021-11-04 11:30:22 +0100

    Use SDK

--- test/unit/date.test.ts
+++ test/unit/date.test.ts
@@ -1,26 +1,21 @@
 import { expect } from 'chai';
-import { createBcmsDateUtility, createBcmsStringUtility } from '../../src';
-
-const stringUtil = createBcmsStringUtility();
-const dateUtil = createBcmsDateUtility({
-  stringUtil,
-});
+import { sdk } from '../util';
 
 describe('Date utility', async () => {
   it('should check if prettyElapsedTimeSince output is "just now"', async () => {
-    const result = dateUtil.prettyElapsedTimeSince(Date.now());
+    const result = sdk.util.date.prettyElapsedTimeSince(Date.now());
     expect(result).to.be.eq('just now');
   });
   it('should check if prettyElapsedTimeSince output is "2 minutes ago"', async () => {
-    const result = dateUtil.prettyElapsedTimeSince(Date.now() - 120000);
+    const result = sdk.util.date.prettyElapsedTimeSince(Date.now() - 120000);
     expect(result).to.be.eq('2 minutes ago');
   });
   it('should check if prettyElapsedTimeSince output is "2 hours ago"', async () => {
-    const result = dateUtil.prettyElapsedTimeSince(Date.now() - 7200000);
+    const result = sdk.util.date.prettyElapsedTimeSince(Date.now() - 7200000);
     expect(result).to.be.eq('2 hours ago');
   });
   it('should check if prettyElapsedTimeSince output is "2 days ago"', async () => {
-    const result = dateUtil.prettyElapsedTimeSince(Date.now() - 172800000);
+    const result = sdk.util.date.prettyElapsedTimeSince(Date.now() - 172800000);
     expect(result).to.be.eq('2 days ago');
   });
 });
