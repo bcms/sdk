891018764ebd0f8de2a1e51aa6068c150427bded
Luka <lukagrujic80@gmail.com>
2021-11-05 12:17:05 +0100

    color tests

--- test/integration/color.test.ts
+++ test/integration/color.test.ts
@@ -4,17 +4,22 @@ import { Login, ObjectUtil, sdk } from '../util';
 describe('Color API', async () => {
   Login();
   let idColor: string;
+  let cIdColor: string;
   let sourceId: string;
   let sourceType: string;
-  it('should create new Color', async () => {
+  it('should create new color', async () => {
     const result = await sdk.color.create({
       label: 'black',
       value: '#030504',
       source: {
-        id: '6177ee6d1059eafcacd1cf00',
+        id: '618504c1d114b206f56fd6fe',
         type: 'template',
       },
     });
+    idColor = result._id;
+    cIdColor = result.cid;
+    sourceId = result.source.id;
+    sourceType = result.source.type;
     expect(result).to.be.instanceOf(Object);
     expect(result).to.have.property('_id').to.be.a('string');
     expect(result).to.have.property('createdAt').to.be.a('number');
@@ -27,15 +32,12 @@ describe('Color API', async () => {
         name: 'black',
         value: '#030504',
         userId: '111111111111111111111111',
-        source: { id: '6177ee6d1059eafcacd1cf00', type: 'template' },
+        source: { id: '618504c1d114b206f56fd6fe', type: 'template' },
       },
       'color',
     );
-    idColor = result._id;
-    sourceId = result.source.id;
-    sourceType = result.source.type;
   });
-  it('should update Color', async () => {
+  it('should be able to update color', async () => {
     const result = await sdk.color.update({
       _id: idColor,
       label: 'Blue',
@@ -58,7 +60,7 @@ describe('Color API', async () => {
       'color',
     );
   });
-  it('should show all Colors', async () => {
+  it('should be to get all colors', async () => {
     const results = await sdk.color.getAll();
     expect(results).to.be.a('array');
     expect(results.length).gte(0);
@@ -76,20 +78,20 @@ describe('Color API', async () => {
       expect(result).to.have.property('userId').to.be.a('string');
     }
   });
-  it('should show many Colors', async () => {
-    const manyId = [idColor, idColor];
-    expect(manyId).to.be.a('array');
-    const results = await sdk.color.getMany(manyId);
+  it('should get many colors in 1 request', async () => {
+    const manyCId = [cIdColor, cIdColor];
+    expect(manyCId).to.be.a('array');
+    const results = await sdk.color.getMany(manyCId);
     expect(results).to.be.a('array');
     expect(results.length).gte(0);
     for (let i = 0; i < results.length; i++) {
-      for (let j = 0; j < manyId.length; j++) {
+      for (let j = 0; j < manyCId.length; j++) {
         const result = results[i];
         expect(result).to.be.instanceOf(Object);
-        expect(result).to.have.property('_id').to.be.a('string').eq(manyId[j]);
+        expect(result).to.have.property('_id').to.be.a('string');
         expect(result).to.have.property('createdAt').to.be.a('number');
         expect(result).to.have.property('updatedAt').to.be.a('number');
-        expect(result).to.have.property('cid').to.be.a('string');
+        expect(result).to.have.property('cid').to.be.a('string').eq(manyCId[j]);
         expect(result).to.have.property('label').to.be.a('string');
         expect(result).to.have.property('name').to.be.a('string');
         expect(result).to.have.property('value').to.be.a('string');
@@ -117,7 +119,26 @@ describe('Color API', async () => {
     expect(result).to.have.property('source').to.be.a('object');
     expect(result).to.have.property('userId').to.be.a('string');
   });
-  it('should delete Color', async () => {
+  it('should fail when trying to get a color which does not exist', async () => {
+    // eslint-disable-next-line no-unused-expressions
+    try {
+      const result = await sdk.color.get('6184f06acbf8c33bfe92b042');
+
+      expect(result).to.be.instanceOf(Object);
+      expect(result).to.have.property('_id').to.be.a('string').eq(idColor);
+      expect(result).to.have.property('createdAt').to.be.a('number');
+      expect(result).to.have.property('updatedAt').to.be.a('number');
+      expect(result).to.have.property('cid').to.be.a('string');
+      expect(result).to.have.property('label').to.be.a('string');
+      expect(result).to.have.property('name').to.be.a('string');
+      expect(result).to.have.property('value').to.be.a('string');
+      expect(result).to.have.property('source').to.be.a('object');
+      expect(result).to.have.property('userId').to.be.a('string');
+    } catch (error) {
+      expect(error).to.be.an('object').to.have.property('code').to.eq('col001');
+    }
+  });
+  it('should be able to delete a color', async () => {
     // eslint-disable-next-line no-unused-expressions
     expect(idColor).to.be.a('string');
     const result = await sdk.color.deleteById(idColor);
