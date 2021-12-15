64b125383f398896d72a485ffe63a2e2612d06be
Bbanez <banezprod@gmail.com>
2021-12-15 12:16:57 +0100

    Improve return

--- src/handlers/change.ts
+++ src/handlers/change.ts
@@ -1,23 +1,18 @@
-import type {
-    BCMSChangeHandlerConfig,
-    BCMSChangeHandler,
-    GetInfoData,
-  } from '../types';
-  
-  export function createBcmsChangeHandler({
-    send,
-  }: BCMSChangeHandlerConfig): BCMSChangeHandler {
-    const baseUri = '/changes';
-    return {
-      async getInfo() {
-        const result: { item: GetInfoData } = await send({
-          url: `${baseUri}/info`,
-          method: 'GET',
-          headers: {
-            Authorization: '',
-          },
-        });
-        return result.item;
-      },
-    };
-  }
+import type { BCMSChangeHandlerConfig, BCMSChangeHandler } from '../types';
+
+export function createBcmsChangeHandler({
+  send,
+}: BCMSChangeHandlerConfig): BCMSChangeHandler {
+  const baseUri = '/changes';
+  return {
+    async getInfo() {
+      return await send({
+        url: `${baseUri}/info`,
+        method: 'GET',
+        headers: {
+          Authorization: '',
+        },
+      });
+    },
+  };
+}
